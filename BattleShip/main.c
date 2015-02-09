10319c2529d1b19b78934492553f4c9f65fdd589
Jacob Andreou <jacobandreou@gmail.com>
2015-02-09 17:30:15 -0500

    Initial Commit

---
+++ BattleShip/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BattleShip
+//
+//  Created by Jacob Andreou on 2015-02-09.
+//  Copyright (c) 2015 TA Labs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
