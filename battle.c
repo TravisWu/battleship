3e0c1ec87fdd35e5ce0edf29e7992ff659a3541b
Raymond Chung <raymond_chung2@hotmail.com>
2015-02-09 00:17:53 -0500

    Fixed bug when Y was not showing! Also added the reset throughout the game and fixed the attempt counter to properly show 19 and not 20 on first turn.

--- battle.c
+++ battle.c
@@ -1,24 +1,18 @@
-	#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 
 void buildBoard(int board[][5])
 {
-	int row;
-	int column;
-
-	for(row=0 ; row <5 ; row++){
-		for(column=0; column <5; column ++){
+	int row, column;
+	for(row=0 ; row <5 ; row++)
+		for(column=0; column <5; column ++)
 			board[row][column] = -1;
-		}
-	}
-
 }
 
 void displayBoard(int board[][5])
 {
-	int row;
-	int column;
+	int row, column;
 
 	for (row= 0; row <5; row++)
 	{
@@ -53,12 +47,10 @@ void displayBoard(int board[][5])
 	}
 			
 }
-
 void makeShips(int ships[][2])
 {
 	srand(time(NULL));
-	int ship;
-	int last;
+	int ship, last;
 
 	for(ship = 0 ; ship<3 ; ship++)
 	{
@@ -66,20 +58,20 @@ void makeShips(int ships[][2])
 		ships[ship][1] = rand() %5; 
 
 		for(last=0; last < ship ; last++){
-			if ((ships[ship][0] == ships[last][0])&& (ships[ship][1] == ships[last][1])){
+			if ((ships[ship][0] == ships[last][0])&& (ships[ship][1] == ships[last][1]))
 				do
-				{
+			{
 					ships[ship][0] = rand()%5;
 					ships[ship][1] = rand()%5; 
 
-				}while( (ships[ship][0] == ships[last][0])&&(ships[ship][1] == ships[last][1]) );
-
-			}
+				}
+				while( (ships[ship][0] == ships[last][0])&&(ships[ship][1] == ships[last][1]) );
 		}
 	}
 }
 
 void shoot (int shot[2]) {
+	printf("Enter 0 at any time to restart or exit.\n");
 	shot[0] = theRow();
 	shot[0]--;
 	shot[1] = column();
@@ -88,17 +80,29 @@ void shoot (int shot[2]) {
 
 int theRow() {
 	int row;
+	int ta;
+	int check;
 	printf("Enter the row you would like to target: ");
-	int check = scanf("%d", &row);
+	check = scanf("%d", &row);
 
-	if(check == 1){
+	if (check == 1) {
+		if (row == 0) {
+			printf("\n Would you like to reset or exit? (1 for Reset, Any key for Exit) ");
+			scanf("%d", &ta);
+			if (ta == 1) {
+				main();
+			} else {
+				printf("Thanks for playing!\n");
+				exit(0);
+			}
+		}
 		if ((row > 5) || (row < 1)) {
 			printf("Out of range! You must choose between 1 and 5.\n");
 			getchar();
 			row = theRow();
-		}
-	}else{
-		printf("Please enter a number\n");
+		} 
+	} else {
+		printf("Not an integer! Please enter a number\n");
 		getchar();
 		row = theRow();
 	}
@@ -107,17 +111,27 @@ int theRow() {
 
 int column() {
 	int col;
+	int ta;
+	int check;
 	printf("Enter the column you would like to target: ");
-	int check = scanf("%d", &col);
-
-	if(check == 1){
+	check = scanf("%d", &col);	
+	if (check == 1) {
+		if (col == 0) {
+			printf("\n Would you like to reset or exit? (1 for Reset, Any key for Exit) ");
+			scanf("%d", &ta);
+			if (ta == 1) {
+				main();
+			} else {
+				printf("Thanks for playing!\n");
+				exit(0);
+			}
+		}
 		if ((col > 5) || (col < 1)) {
 			printf("Out of range! You must choose between 1 and 5.\n");
-			getchar();
 			col = column();
 		}
-	}else{
-		printf("Please enter a number\n");
+	} else {
+		printf("Not an integer! Please enter a number\n");
 		getchar();
 		col = column();
 	}
@@ -128,44 +142,41 @@ int hit(int shot[2], int ships[][2])
 {
     int ship;
 
-    for(ship=0 ; ship < 3 ; ship++){
-        if( shot[0]==ships[ship][0] && shot[1]==ships[ship][1])
-        {
-            printf("boom (%d,%d)\n",shot[0]+1,shot[1]+1);
-            return 1;
+        for(ship=0 ; ship < 3 ; ship++){
+            if( shot[0]==ships[ship][0] && shot[1]==ships[ship][1])
+            {
+                printf("Boom (%d,%d)\n",shot[0]+1,shot[1]+1);
+                return 1;
+            }
         }
+        return 0;
     }
-    return 0;
-}
-
-void tip(int shot[2], int ships[][2], int attempt)
-{
-	system("clear");
-	int counter = 20;
-	int row = 0;
-	int column =0;
-	int sub;
+ void tip(int shot[2], int ships[][2], int attempt)
+ {
+ 	system("clear");
+ 	int counter = 19;
+ 	int row = 0;
+ 	int column =0;
+ 	int sub;
 
 	printf("You have %d attempts left.\n", (counter-attempt));
-	for(sub=0 ; sub <3 ; sub ++)
-	{
-		if (ships[sub][0] == shot[0])
-			row++;
-		if (ships[row][1] == shot[1])
-			column++; 
-	}	
-	printf("\nturn %d: \nrow %d : %d ships\ncolumn %d : %d ships\n",(attempt+1),shot[0]+1,row,shot[1]+1,column);
+ 	for(sub=0 ; sub <3 ; sub ++)
+ 	{
+ 		if (ships[sub][0] == shot[0])
+ 			row++;
+ 		if (ships[row][1] == shot[1])
+ 			column++; 
+ 	}	
+ 	printf("\nTurn %d: \nRow %d : %d ships\nColumn %d : %d ships\n",(attempt+1),shot[0]+1,row,shot[1]+1,column);
 
-}
-
-void refreshBoard(int shot[2], int ships[][2], int board[][5])
-{
-    if(hit(shot,ships)){
-        board[shot[0]][shot[1]]=1;
-    }else{
-        board[shot[0]][shot[1]]=0;
+ }
+ void refreshBoard(int shot[2], int ships[][2], int board[][5])
+ {
+        if(hit(shot,ships))
+            board[shot[0]][shot[1]]=1;
+        else
+            board[shot[0]][shot[1]]=0;
     }
-}
 
 
 int main()
@@ -176,50 +187,47 @@ int main()
 	int shot[2];
 	int attempts = 0;
 	int hits = 0;
-	int counter = 20;
+	int counter = 19;
 	int ta;
 	buildBoard(board);
 	makeShips(ships);
-
 	printf("You have %d attempts left.\n", (counter-attempts));
 	printf("\n");
-	
 	do 
 	{ 	
 		displayBoard(board);
 		shoot(shot);
 		attempts++;
-		if (hit(shot,ships)){	
+		if (hit(shot,ships))
+		{	
 			tip(shot, ships, attempts);
 			hits++ ;
-		}else{
+		}
+		else
 			tip(shot, ships, attempts); 
 			refreshBoard(shot,ships,board);
 
 			if (attempts == counter) {
-				printf ("Game Over!\n");
-				printf("\n Try again? (1 for Yes, 2 for No) ");
-				scanf("%d", &ta);
-				if (ta == 1) {
-					main();
-				} else {
-					exit(0);
-				}
+			printf ("Game Over!\n");
+			printf("\n Would you like to try again? (1 for Yes, Any key for No) ");
+			scanf("%d", &ta);
+			if (ta == 1) {
+				main();
+			} else {
+				printf("Thanks for playing!\n");
+				exit(0);
 			}
-
 		}
-	
-	}while (hits!=3);
-
-	printf("\n finished. You hit all 3 ships in %d attempts\n", attempts);
+	}
+	while (hits!=3);
+	printf("\n Mazel Tov! You hit all 3 ships in %d attempts\n", attempts);
 	displayBoard(board);
-	
-	printf("\n Try again? (1 for Yes, 2 for No) ");
+	printf("\n Would you like to try again? (1 for Yes, Any key for No) ");
 	scanf("%d", &ta);
 	if (ta == 1) {
 		main();
 	} else {
+		printf("Thanks for playing!\n");
 		exit(0);
 	}
-
 }
