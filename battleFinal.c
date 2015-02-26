dfc4509aee28cc8338089f12aeb6ebf0570d4067
Enoch Tam <et.enoch@gmail.com>
2015-02-26 01:01:51 -0500

    commented c file

--- battleFinal.c
+++ battleFinal.c
@@ -2,30 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-int theRow();
+// forward declarations
+int theRow(); 
 int rowCheck();
 int column();
 void shoot(int*);
 void makeShips(int(*)[2]);
 int main();
 
+
+// buildBuild creates a 2d array to represent the game board
 void buildBoard(int board[][5]) {
 	int row;
 	int column;
-	for(row=0 ; row <5 ; row++)
+	for(row=0 ; row <5 ; row++) // create the 5 x 5 board
 		for(column=0; column <5; column ++)
-			board[row][column] = -1;
+			board[row][column] = -1; // initialize board with -1
 }
 
-
+// displayBoard draws an ascii representation of the board
 void displayBoard(int board[][5], int choice) {
 	int row;
 	int column;
 
-	char* miss = " X |";
+	char* miss = " X |"; // game markers
 	char* hit = " Y |";
 
-	if (choice == 1) {
+	if (choice == 1) { // decide what game markers to use on the board
 		miss = " O |";
 		hit = " x |";		
 	} else if (choice == 2) {
@@ -37,14 +40,14 @@ void displayBoard(int board[][5], int choice) {
 	}
 
 	for (row= 0; row <5; row++) {
-		if(row == 0) {
-			printf("   1   2   3   4   5 \n");
+		if(row == 0) { // print top of board
+			printf("   1   2   3   4   5 \n"); 
 			printf(" *-------------------*\n");
 		}
 
 		printf("%d|",row+1);
 
-		for (column = 0; column <5 ; column++) {
+		for (column = 0; column <5 ; column++) { // print corrent symbol for current spot
 			if(board[row][column]== -1)	{
 				printf("   |");
 			} 
@@ -56,26 +59,27 @@ void displayBoard(int board[][5], int choice) {
 			}
 		}
 
-		if (row == 4)  
+		if (row == 4)  // end of board, print bottom row
 			printf("\n *-------------------*\n");
 		else
 			printf("\n |---|---|---|---|---|\n");
 	}		
 }
 
+// makeShips creates three randomly placed "ships" on the board
 void makeShips(int ships[][2]) {
-	srand(time(NULL));
+	srand(time(NULL)); // seed random number generator
 	int ship;
 	int last;
 
 	for(ship = 0 ; ship<3 ; ship++) {
-		ships[ship][0] = rand() %5; 
-		ships[ship][1] = rand() %5; 
+		ships[ship][0] = rand() %5; // row
+		ships[ship][1] = rand() %5; // column
 
-		for(last=0; last < ship ; last++){
+		for(last=0; last < ship ; last++){ // make sure there are no duplicates
 			if ((ships[ship][0] == ships[last][0])&& (ships[ship][1] == ships[last][1])){
-				do {
-					ships[ship][0] = rand()%5;
+				do { // if duplicates, try new ships
+					ships[ship][0] = rand()%5; 
 					ships[ship][1] = rand()%5; 
 
 				}while ( (ships[ship][0] == ships[last][0])&&(ships[ship][1] == ships[last][1]) );
@@ -84,6 +88,7 @@ void makeShips(int ships[][2]) {
 	}
 }
 
+// shoot function promts user to take a "shot"
 void shoot (int shot[2]) {
 	printf("Enter 0 at any time to restart or exit.\n");
 	shot[0] = theRow();
@@ -92,16 +97,17 @@ void shoot (int shot[2]) {
 	shot[1]--;
 }
 
+// theRow captures input for the row coordinate 
 int theRow() {
-	int row;
+	int row; 
 	int ta;
 	int check;
 	int info;
 	printf("Enter the row you would like to target: ");
 	check = scanf("%d", &row);
 
-	if (check == 1) {
-		if (row == 0) {
+	if (check == 1) { // scanf found 1 integer
+		if (row == 0) { // validate ipnut
 			printf("\n Would you like to reset or exit? (1 for Reset, Any key for Exit) ");
 			scanf("%d", &ta);
 			if (ta == 1) {
@@ -117,14 +123,15 @@ int theRow() {
 			info = rowCheck();
 			row = theRow();
 		} 
-	} else {
+	} else {// scanf didn't find anything
 		printf("Not an integer! Please enter a number\n");
 		getchar();
 		row = theRow();
 	}
-	return row;
+	return row;// will return row if not conditions met above (sucess)
 }
 
+// theRow captures input for the row coordinate 
 int column() {
 	int col;
 	int ta;
@@ -132,8 +139,8 @@ int column() {
 
 	printf("Enter the column you would like to target: ");
 	check = scanf("%d", &col);	
-	if (check == 1) {
-		if (col == 0) {
+	if (check == 1) {// ensure scanf found 1 integer
+		if (col == 0) { // validate ipnut
 			printf("\n Would you like to reset or exit? (1 for Reset, Any key for Exit) ");
 			scanf("%d", &ta);
 			if (ta == 1) {
@@ -147,18 +154,19 @@ int column() {
 			printf("Out of range! You must choose between 1 and 5.\n");
 			col = column();
 		}
-	} else {
+	} else { // scanf didn't find anything
 		printf("Not an integer! Please enter a number\n");
 		getchar();
 		col = column();
 	}
-	return col;
+	return col;// will return column if not conditions met above (sucess)
 }
 
+// hit will return 1/true if a shot hit a ship
 int hit(int shot[2], int ships[][2]) {
 	int ship;
 
-    for(ship=0 ; ship < 3 ; ship++){
+    for(ship=0 ; ship < 3 ; ship++){ // check ships against the current shot
         if( shot[0]==ships[ship][0] && shot[1]==ships[ship][1]) {
             printf("Boom (%d,%d)\n",shot[0]+1,shot[1]+1);
             return 1;
@@ -167,6 +175,7 @@ int hit(int shot[2], int ships[][2]) {
     return 0;
 }
 
+// tip generates a "tip" for the user
 void tip(int shot[2], int ships[][2], int attempt,int counter) {
 	system("clear");
 	int row = 0;
@@ -174,6 +183,7 @@ void tip(int shot[2], int ships[][2], int attempt,int counter) {
 	int sub;
 
 	printf("You have %d attempts left.\n", (counter-attempt));
+	// the number of ships in the current row or column is printed
 	for(sub=0 ; sub <3 ; sub ++) {
 		if (ships[sub][0] == shot[0])
 			row++;
@@ -183,6 +193,7 @@ void tip(int shot[2], int ships[][2], int attempt,int counter) {
 	printf("\nTurn %d: \nRow %d : %d ships\nColumn %d : %d ships\n",(attempt+1),shot[0]+1,row,shot[1]+1,column);
 }
 
+// refreshBoard modifies the board 2d array apropriately
 void refreshBoard(int shot[2], int ships[][2], int board[][5]) {
     if(hit(shot,ships))
         board[shot[0]][shot[1]]=1;
@@ -190,11 +201,13 @@ void refreshBoard(int shot[2], int ships[][2], int board[][5]) {
         board[shot[0]][shot[1]]=0;
 }
 
+// program main function
 int main() {
 	int check;
 	int choice;
 	int counter = 0;
 	
+	// game setup
 	system("clear");
 	printf("Welcome to BattleShip!\n");
 	printf("******** GAME SETUP ********\n");
@@ -215,19 +228,20 @@ int main() {
 		getchar();
 	}
 
+	// begin game process
 	if ((choice == 1) || (choice == 2) || (choice == 3) || (choice == 4)) {
 		system("clear");
-		int board[5][5];
+		int board[5][5]; // declare game variables
 		int ships[3][2];
 		int shot[2];
 		int attempts = 0;
 		int hits = 0;
 		int ta;
-		buildBoard(board);
+		buildBoard(board);// call game functions
 		makeShips(ships);
 		printf("You have %d attempts left.\n", (counter-attempts));
 		printf("\n");
-		do {
+		do { // game loop
 
 			displayBoard(board, choice);
 			shoot(shot);
@@ -239,19 +253,19 @@ int main() {
 				tip(shot, ships, attempts,counter); 
 
 			refreshBoard(shot,ships,board);
-			if (attempts == counter) {
+			if (attempts == counter) { // exceeded max attempts
 				printf ("Game Over!\n");
 				printf("\n Would you like to try again? (1 for Yes, Any key for No) ");
 				scanf("%d", &ta);
 				if (ta == 1) {
-					main();
+					main(); // repeat game
 				} else {
 					printf("Thanks for playing!\n");
 					exit(0);
 				}
 			}
 
-		}while (hits!=3);
+		}while (hits!=3); // have not hit all 3 ships
 
 		printf("\n Mazel Tov! You hit all 3 ships in %d attempts\n", attempts);
 		displayBoard(board, choice);
@@ -269,6 +283,7 @@ int main() {
 	}
 }
 
+// rowCheck captures user input and checks for actions (exit,restart)
 int rowCheck () {
 	int rowAns;
 	int reAns;
